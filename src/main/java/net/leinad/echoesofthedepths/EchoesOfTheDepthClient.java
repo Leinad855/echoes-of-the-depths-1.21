049fe1b4c3e0c78c6469a6573d51a38bf785c91b
Leinad855 <leinad855pato@gmail.com>
2025-11-14 20:12:59 +0100

    Initial commit

---
+++ src/main/java/net/leinad/echoesofthedepths/EchoesOfTheDepthClient.java
@@ -0,0 +1,10 @@
+package net.leinad.echoesofthedepths;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchoesOfTheDepthClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
