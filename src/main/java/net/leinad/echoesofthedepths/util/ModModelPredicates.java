aeb508a05c53a73f4f42c05bc5298076f9f1bbe6
Leinad855 <leinad855pato@gmail.com>
2025-12-24 10:38:31 +0100

    Resonite Axe

--- src/main/java/net/leinad/echoesofthedepths/util/ModModelPredicates.java
+++ src/main/java/net/leinad/echoesofthedepths/util/ModModelPredicates.java
@@ -2,6 +2,7 @@ package net.leinad.echoesofthedepths.util;
 
 import net.leinad.echoesofthedepths.EchoesOfTheDepths;
 import net.leinad.echoesofthedepths.Item.ModItems;
+import net.leinad.echoesofthedepths.Item.custom.ResoniteAxeItem;
 import net.leinad.echoesofthedepths.Item.custom.ResonitePickaxeItem;
 import net.leinad.echoesofthedepths.component.ModDataComponentsType;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
@@ -16,5 +17,9 @@ public class ModModelPredicates {
         ModelPredicateProviderRegistry.register(ModItems.RESONITE_PICKAXE, Identifier.of(EchoesOfTheDepths.MOD_ID, "charged"),
                 (stack, world, entity, seed) -> stack.get(ModDataComponentsType.CHARGE) != null
                         && stack.get(ModDataComponentsType.CHARGE) == ResonitePickaxeItem.MAX_CHARGE ? 1f : 0f);
+
+        ModelPredicateProviderRegistry.register(ModItems.RESONITE_AXE, Identifier.of(EchoesOfTheDepths.MOD_ID, "charged"),
+                (stack, world, entity, seed) -> stack.get(ModDataComponentsType.CHARGE) != null
+                        && stack.get(ModDataComponentsType.CHARGE) == ResoniteAxeItem.MAX_CHARGE ? 1f : 0f);
     }
 }
